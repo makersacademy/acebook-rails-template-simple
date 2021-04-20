d7b2751a9bf2aab5c648248388610bbd5950e310
Louise Ballantyne <lousballantyne@icloud.com>
2021-04-20 15:39:52 +0100

    Added a further comment to explain code to add_user_to_posts.rb

--- db/migrate/20210420141610_add_user_to_posts.rb
+++ db/migrate/20210420141610_add_user_to_posts.rb
@@ -1,5 +1,7 @@
 class AddUserToPosts < ActiveRecord::Migration[6.1]
   def change
+    # Adds a foreign key 'user_id' to the posts table, referencing id in the users table
+    # add_foreign_key(from_table, to_table, options): Adds a new foreign key. from_table is the table with the key column, to_table contains the referenced primary key.
     add_reference :posts, :user, foreign_key: { to_table: :users}
   end
 end
