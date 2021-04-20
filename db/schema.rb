0ab1b5976ba0bcee06c0938d16d4bf8d68774ebe
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2021-04-20 16:20:13 +0100

    added likes database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_141610) do
+ActiveRecord::Schema.define(version: 2021_04_20_151631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
@@ -34,5 +43,7 @@ ActiveRecord::Schema.define(version: 2021_04_20_141610) do
     t.string "profilePicture"
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
