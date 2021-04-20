9db5cc848adabf97326decc40020a48334af41e1
Will Spencer <willspencer16@gmail.com>
2021-04-20 16:20:17 +0100

    Added sessions destroy method and current_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'post#index'
+  
   resources :posts
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
