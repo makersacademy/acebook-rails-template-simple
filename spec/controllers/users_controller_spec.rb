7c4578f2e91043ca750d958c0ae25f71c88c79b3
jackmcc08 <jackmcc08@gmail.com>
2021-04-20 16:10:15 +0100

    created tests for user sign up, 1 feature test and 4 unit tests based off the posts tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe "GET /users/new " do
+    it "responds with 200" do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /users/create" do
+    it "responds with 200" do
+      post :create, params: { user: { username: "Sha88", password: "Password88" } }
+      expect(response).to redirect_to(posts_url)
+    end
+
+    it "creates a user" do
+      post :create, params: { user: { username: "Sha88", password: "Password88" } }
+      expect(Post.find_by(message: "Hello, Sha88!")).to be
+      # Is this test meant to end like this? 
+    end
+  end
+end
---
+++ spec/features/user_can_sign_up_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "SignUp", type: :feature do
+  scenario "User can sign up to acebook" do
+    visit "/"
+    click_button "Sign Up"
+    fill_in "Username", with: "Sha88"
+    fill_in "Password", with: "Password88"
+    click_button "Submit"
+    expect(page).to have_content("Hello, Sha88!")
+  end
+end
