27ec36dcf9300d9cc6b469916cfd225dabe1b3d7
jackmcc08 <jackmcc08@gmail.com>
2021-04-22 17:43:04 +0100

    got new comments to redirect back to the comments page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,19 @@
 class CommentsController < ApplicationController
   def new
-    @comment = Comment.new
+    puts "is this what you see"
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
   end
 
   def create
     @post = Post.find(params[:post_id])
+    puts "does it get here"
     @comment = @post.comments.create(comment_params)
-    redirect_to post_comments(@post)
+    puts "does it get here 2"
+    # WHY ARE YOU NOT REDIRECTING ANYWHERE
+    # get :index, params: { :post_id => post.id }
+    redirect_to post_comments_path(@post)
+    # render :index
   end
 
   def index
@@ -16,6 +23,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-      params.require(:comment).permit(:user, :body, :post)
+      params.require(:comment).permit(:user_id, :body, :post_id)
   end
 end
