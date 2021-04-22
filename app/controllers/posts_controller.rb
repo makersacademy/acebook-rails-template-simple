9d65bdaa71cec53df3f510e54d63b5906e42c85e
Sha <cspoppuppy@hotmail.com>
2021-04-22 12:30:02 +0100

    post with username
    Co-authored-by David <dm_greaves@msn.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,18 +4,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    p "----------------"
-    p post_params
-
     @post = Post.create(post_params)
     redirect_to posts_url(@post)
   end
 
   def index
-    @user = User.find_by_id(session[:user_id])
-    session[:user_id] = nil # this is framework code to be refactored out, it kills the session automatically after sign - up this is purely to show it works.
-    # session seems to maintain even after closing the server or closing the page
-    @posts = Post.order('created_at').reverse_order
+    @posts = Post.order('created_at': :desc)
   end
 
   private
--- spec/features/named_posts_feature_spec.rb
+++ spec/features/named_posts_feature_spec.rb
@@ -2,19 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Posts display the username of poster" do
-
-    # visit "/posts"
-    # click_button "Sign up"
-    # fill_in "Username" with: "David"
-    # fill_in "Password" with: "1234"
-    # click_button "Sign up"
     #
     # visit "/posts"
     # fill_in "Username" with: "David"
     # fill_in "Password" with: "1234"
     # click_button "Sign in"
 
-    post_message("Today is Sunny!")
+    post_message("Today is Sunny!", "David")
 
     expect(page).to have_content("Posted by: David")
     expect(first('.post')).to have_content('David')
