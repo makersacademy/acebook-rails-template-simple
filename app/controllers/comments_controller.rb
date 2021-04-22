4902d070b13f09fef624493f74116fe0ada70f90
Will Spencer <willspencer16@gmail.com>
2021-04-22 16:20:48 +0100

    Created a list of comments relating to a post and a new comments link. Over to Jack to fix.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,21 @@
 class CommentsController < ApplicationController
-  # def create
-  #     # @post = Post.find(params[:post_id])
-  #     @comment = Comment.create(comment_params)
-  #     redirect_to post_comments
-  # end
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_comments(@post)
+  end
 
   def index
     @post = Post.find(params[:post_id])
     @comments = @post.comments.order('created_at').reverse_order
   end
 
-  # private
-  # def comment_params
-  #   params.require(:comment).permit(:user, :body, :post)
-  # end
+  private
+  def comment_params
+      params.require(:comment).permit(:user, :body, :post)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,16 +18,3 @@ class PostsController < ApplicationController
     params.require(:post).permit(:message, :user_id)
   end
 end
-
-# RSpec.describe "Widget management", :type => :request do
-#   it "creates a Widget and redirects to the Widget's page" do
-#     get "/widgets/new"
-
-#     expect(response).to have_http_status(:ok)
-
-#     post "/widgets", :params => { :widget => {:name => "My Widget"} }
-#     expect(response).to have_http_status(302)
-#     follow_redirect!
-#     expect(response).to have_http_status(:success)
-#   end
-# end
