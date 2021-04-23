bf30512320984ae00438385d174ecb2857260f73
Louise Ballantyne <lousballantyne@icloud.com>
2021-04-23 15:56:44 +0100

    Added changes made from: https://medium.com/swlh/react-reactions-cfdde7f08dff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :forename, presence: true
   has_many :likes, dependent: :destroy
   has_many :posts, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_133854) do
+ActiveRecord::Schema.define(version: 2021_04_23_143828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_133854) do
     t.string "surname"
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "profilePic"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
