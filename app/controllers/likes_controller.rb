9d0b3ab80896eac963a9d28dfbbe020070c474b8
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-04-27 15:55:05 +0100

    fixed like button and added logic so each user can like a post once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,27 @@
 class LikesController < ApplicationController
   before_action :find_post
 
-  # def new
-  #   @likes = Like.new
-  # end
+  def new
+
+  end
 
   def create
-    @post.likes.create(user_id: current_user.id)
+    if already_liked?
+      flash.now[:like_alert] = "You can't like more than once"
+   else
+     @post.likes.create(user_id: current_user.id)
+  end
     redirect_to root_url
   end
+
   private
+
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id:
+    params[:post_id]).exists?
   end
 end
--- spec/features/user_can_like_a_post_spec.rb
+++ spec/features/user_can_like_a_post_spec.rb
@@ -1,13 +1,32 @@
 require 'rails_helper'
 
-RSpec.feature "Logout", type: :feature do
+RSpec.feature "Like button", type: :feature do
 
-  scenario 'user can like a post' do
+  scenario 'user can only like a post once' do
     visit "/"
     post_message("Shout out to my homiiiieeees", "Cool Aunt Dodo")
     expect(page).to have_content("0 Likes")
     click_button "Like"
-    expect(page).to have_content("1 Likes")
+    expect(page).to have_content("1 Like")
+    click_button "Like"
+    expect(page).to have_content("1 Like")
+    # expect(page).to have_content("You can't like more than once")
+    # expect a flash message
   end
 
+  scenario 'a post can be liked mulitple times by different users' do
+    visit "/"
+    post_message("Shout out to my homiiiieeees", "Cool Aunt Dodo")
+    expect(page).to have_content("0 Likes")
+    click_button "Like"
+    expect(page).to have_content("1 Like")
+    click_link "Log Out"
+    sign_up("Scheming Lovebug")
+    click_button "Like"
+    expect(page).to have_content("2 Likes")
+    click_link "Log Out"
+    sign_up("Smelly Bubbles")
+    click_button "Like"
+    expect(page).to have_content("3 Likes")
+  end
 end
