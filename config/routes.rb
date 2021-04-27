ba74d44bc751a5c83f5a44f29a0556e312365d3a
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-04-27 11:24:50 +0100

    like button working

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  post 'like', to: 'likes#create', as: 'like'
 end
