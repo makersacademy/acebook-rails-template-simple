6b3b2341bb06a1ca82135cbe63a380ca4930be6d
Stuart Baines-Dinning <33100049+StuBehan@users.noreply.github.com>
2021-04-28 16:10:41 +0100

    Update sessions_controller.rb
    Co-Authored-By: Louise Ballantyne <76226969+louballantyne@users.noreply.github.com>
    Co-Authored-By: Inbar Vernia <78118133+inbarvernia@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,20 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: session_params[:username])
-
-    if @user && @user.authenticate(session_params[:password])
+    
+    if @user == nil
+      render json: {
+        logged_in: false, 
+        message: 'That username does not exist',
+        status: 403
+      }
+    elsif @user.authenticate(session_params[:password]) == false
+      render json: {
+        logged_in: false, 
+        message: 'Password is incorrect',
+        status: 403
+      }
+    else
       login!
       render json: {
         logged_in: true,
@@ -17,6 +29,11 @@ class SessionsController < ApplicationController
       render json: {
         logged_in: true,
         user: current_user
+      }
+    else
+      render json: {
+        logged_in: false,
+        status: 401
       }
     end
   end
