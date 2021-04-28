dfc968a9b846155c75941ff6f9842b4a3c98e01b
Will Spencer <willspencer16@gmail.com>
2021-04-28 11:45:39 +0100

    Added helpers and refactored tests

--- spec/features/user_can_like_a_post_spec.rb
+++ spec/features/user_can_like_a_post_spec.rb
@@ -2,29 +2,27 @@ require 'rails_helper'
 
 RSpec.feature "Like button", type: :feature do
 
-  scenario 'user can like and unlike a post' do
-    visit "/"
+  scenario 'multiple users can like and unlike a post' do
     post_message("Shout out to my homiiiieeees", "Cool Aunt Dodo")
     expect(page).to have_content("0 Likes")
-    click_button "Like"
+    like_post
     expect(page).to have_content("1 Like")
-    click_button "Like"
+    like_post
+    expect(page).to have_content("0 Likes")
+    log_out_sign_up_and_like("Han Solo")
+    expect(page).to have_content("1 Like")
+    like_post
     expect(page).to have_content("0 Likes")
   end
 
   scenario 'a post can be liked mulitple times by different users' do
-    visit "/"
     post_message("Shout out to my homiiiieeees", "Cool Aunt Dodo")
     expect(page).to have_content("0 Likes")
-    click_button "Like"
+    like_post
     expect(page).to have_content("1 Like")
-    click_link "Log Out"
-    sign_up("Scheming Lovebug")
-    click_button "Like"
+    log_out_sign_up_and_like("Han Solo")
     expect(page).to have_content("2 Likes")
-    click_link "Log Out"
-    sign_up("Smelly Bubbles")
-    click_button "Like"
+    log_out_sign_up_and_like("Yoda")
     expect(page).to have_content("3 Likes")
   end
 end
--- spec/helpers/likes_helper_spec.rb
+++ spec/helpers/likes_helper_spec.rb
@@ -1,15 +1,11 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the LikesHelper. For example:
-#
-# describe LikesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-RSpec.describe LikesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+def like_post
+  click_button "Like"
+end
+
+def log_out_sign_up_and_like(username)
+  log_out
+  sign_up(username)
+  like_post
 end
