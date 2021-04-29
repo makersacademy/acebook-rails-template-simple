3247b0cc16f6851133b9ee31c7c030b4a4f84d02
jackmcc08 <jackmcc08@gmail.com>
2021-04-29 11:51:28 +0100

    fixed tests, two remaining to fix, lou to add adverts

--- spec/features/posts_display_username_and_photo_spec.rb
+++ spec/features/posts_display_username_and_photo_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Shows username and photo next to post" do
     post_message("Hello, world!", "David")
-    expect(first('.post')).to have_css("img[src*='pikachu.png']")
+    expect(first('.post-div')).to have_css("img[src*='pikachu.png']")
   end
 end
