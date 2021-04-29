e9007c39435823a0cba91df93bd91c8e7869fad2
jackmcc08 <jackmcc08@gmail.com>
2021-04-29 17:12:48 +0100

    added test for users on failure to create user, also added gem to get render_template checker to work

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
+  gem 'rails-controller-testing'
   gem 'rspec-rails', '~> 4.0'
   gem "rubocop", '~> 1.13', require: false
   gem "rubocop-rails"
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -19,5 +19,16 @@ RSpec.describe UsersController, type: :controller do
       expect(User.find_by(username: "Sha88")).to be
       # Is this test meant to end like this?
     end
+
+    it "renders new if user input is incorrect" do
+      post :create, params: { user: { username: "Sha88", password: "Password88", } }
+      expect(response).to have_http_status(200)
+    end
+
+    it "creates a user" do
+      post :create, params: { user: { username: "Sha88", password: "Password88", image: fixture_file_upload('./spec/fixtures/pikachu.png') } }
+      expect(User.find_by(username: "Sha88")).to be
+      # Is this test meant to end like this?
+    end
   end
 end
