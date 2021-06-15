ed9e7688000a5279379b5bb4bbf7ab9b75f1c6a3
charlie-casserly <charliecasserly215@gmail.com>
2021-06-15 16:30:33 +0100

    Feature complete

--- spec/features/user_can_like_posts_spec.rb
+++ spec/features/user_can_like_posts_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and view them" do
+  scenario "Can like posts and amount of likes is viewable next to the post" do 
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
+    expect(page).to have_content("0 likes")
+    click_link "Like"
+    expect(page).to have_content("1 likes")
   end
 end
-
