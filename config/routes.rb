b5a6f64cc2e690e0e7b48a4662d13997d047fe8a
charlie-casserly <charliecasserly215@gmail.com>
2021-06-16 13:40:55 +0100

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts do
-    get '/like', on: :member, to: 'posts#like'
-  end
+  resources :posts
 end
