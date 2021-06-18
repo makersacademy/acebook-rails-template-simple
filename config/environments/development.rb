bbe2823ed596f430cbeeb2b89b78084983ac1eae
Charlie Slater <charliemslater1701@gmail.com>
2021-06-18 17:33:59 +0100

    displays name next to post

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
+  # store files locally
+  config.active_storage.service = :local
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
