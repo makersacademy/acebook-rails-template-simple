7dd34c2fe7b9eb0a71fc8e1655d7ae6c71eb0328
moby-codes <mubashar.jaffri@gmail.com>
2021-09-15 17:20:14 +0100

    trying new form type, commented out initializer in user class

--- app/controllers/eightbook_controller.rb
+++ app/controllers/eightbook_controller.rb
@@ -8,15 +8,16 @@ class EightbookController < ApplicationController
     #create a new user after clicking Sign Up! button
     #create a new user obj saving params to instance variables
     #redirect to 'posts' page 
-    @user = User.new(params[:first_name], params[:last_name], params[:email], params[:password])
+    @user = User.new(post_params)
+    @user.save
     redirect_to posts_url
   
   end
   
 
-  # private 
-  #   def post_params
-  #     params.require(:user).permit(:first_name, :last_name, :email, :password)
-  #   end
+  private 
+    def post_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
 
 end
