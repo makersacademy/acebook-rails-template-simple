4e9b26327158f75fa7ef98d84d0be830c1442d27
William Day <williamjday11@gmail.com>
2021-09-15 17:21:04 +0100

    Added scope restriction to Like class to prevent double likes & updated tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  like = Like.new(11, 22)
-
-  it "has a user id" do
-    expect(like.user_id).to eq(11)
-  end
-
-  it "has a post id" do
-    expect(like.post_id).to eq(22)
-  end
-  
+  it { is_expected.to be }  
 end
