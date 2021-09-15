cfad42e25246fc35bd8fedf9f35ae72c54e9e9a1
ian k <kickthechair@gmail.com>
2021-09-15 18:00:28 +0100

    active record working

--- app/controllers/eightbook_controller.rb
+++ app/controllers/eightbook_controller.rb
@@ -8,16 +8,9 @@ class EightbookController < ApplicationController
     #create a new user after clicking Sign Up! button
     #create a new user obj saving params to instance variables
     #redirect to 'posts' page 
-    @user = User.new(post_params)
+    @user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password_digest: params[:password_digest])
     @user.save
     redirect_to posts_url
-  
   end
   
-
-  private 
-    def post_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password)
-    end
-
 end
