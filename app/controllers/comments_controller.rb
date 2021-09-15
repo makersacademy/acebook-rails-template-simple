d7104d7e94f11fe849dd1bbf4d1a693497eb4507
Farya <farya.hsn@gmail.com>
2021-09-15 18:32:40 +0200

    adding posts and comments - some features need to be worked on, but general functionality is there :)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,13 @@
 class CommentsController < ApplicationController
-def create
-    @comment = Comment.new(post_params)
-    @comment.save
-    # redirect_to @post
-    redirect_to posts_path
-        #:action: "show", id: 1
-end
-private def post_params
-    params.require(:comment).permit(:comment, :post_id)
-end
+
+    def create
+        @comment = Comment.new(post_params)
+        @comment.save
+        redirect_to posts_path
+    end
+
+    private def post_params
+        params.require(:comment).permit(:comment, :post_id)
+    end
+
 end
--- db/migrate/20210915080020_create_posts.rb
+++ db/migrate/20210915080020_create_posts.rb
@@ -3,9 +3,6 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.integer :user_id
       t.text :post_content
-      t.datetime :created_at
-
-      # t.timestamps
     end
   end
 end
