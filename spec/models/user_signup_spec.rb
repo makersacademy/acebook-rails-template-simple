27a4197bd14e742380a9cbaf1c3e8c55f1efd777
ian k <kickthechair@gmail.com>
2021-09-15 14:14:52 +0100

    added user class

--- spec/models/user_signup_spec.rb
+++ spec/models/user_signup_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe User, type: :model do
   let(:user) {described_class.new('John', 'Wick', 'kissmyguns@test.com', 'test')}
   describe 'user instantiation' do
     it 'has a first name, last name, password and email' do
-      expect(user.first_name).to_eq 'John'
-      expect(user.last_name).to_eq 'Wick'
-      expect(user.email).to_eq 'kissmyguns@test.com'
-      expect(user.password).to_eq 'test'
+      expect(user.first_name).to eq 'John'
+      expect(user.last_name).to eq 'Wick'
+      expect(user.email).to eq 'kissmyguns@test.com'
+      expect(user.password).to eq 'test'
     end
   end
 end
