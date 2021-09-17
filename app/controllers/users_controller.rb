4c0a09204e9d90e5aa7e8a05ae03f194f9387511
ian k <kickthechair@gmail.com>
2021-09-17 12:04:28 +0100

    pathing issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,19 @@
 class UsersController < ApplicationController
   
   def index
-    #show homepage - user sign up
+    
   end
 
   def create
-    #create a new user after clicking Sign Up! button
-    #create a new user obj saving params to instance variables
-    #redirect to 'posts' page 
-    @user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password_digest: params[:password_digest])
+    @user = User.new(user_params)
     @user.save
     redirect_to posts_url
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "users#index"
+  root to: "users#index"
 
   resources :posts, :users
+  post “/users”, to: “users#create”
   
 end
