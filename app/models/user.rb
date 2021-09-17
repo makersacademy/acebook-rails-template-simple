96a3f4321df7d7ca36f2938ad00536c4ea216efe
eballantine <lizzy.kayballantine@gmail.com>
2021-09-17 17:17:18 +0100

    FIX: incorrect password reference in user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true, length: { in: 8..40 }
+  validates :password, presence: true, length: { in: 8..40 }
 
   has_secure_password
 end
