a9d947bb11139a4011353f9a8dfb9daeb81852fc
Blessing Ubogu <60205509+BlessingUb@users.noreply.github.com>
2021-09-21 17:49:12 +0100

    Unathenticated users cannot interact with the posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,22 @@
 class PostsController < ApplicationController
   def new
+    redirect_to sign_in_path unless session[:user_id]
     @post = Post.new
   end
 
   def index
+    redirect_to sign_in_path unless session[:user_id]
     @pagy, @posts = pagy(Post.all.order(created_at: :desc), items: 5)
     @like = Like.new
   end
 
   def show
+   redirect_to sign_in_path unless session[:user_id]
    @post = Post.find(params[:id])
   end
 
   def create
+    redirect_to sign_in_path unless session[:user_id]
     @post = Post.new(post_params)
     @post.save
     redirect_to posts_path
