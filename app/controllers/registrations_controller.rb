0338fc0e27f3134651fe8cac3ba97a1494545dee
Leah <leahpitter95@gmail.com>
2021-10-14 14:53:13 +0100

    able to save a new user to the database using signup form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,17 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    render plain: "Thanks!"
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'successfully created account'
+    else
+      render :new
+    end  
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end 
 
