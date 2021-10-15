8189ec87d5dfe5b96884a7793a46754187ba169e
Osneil Drakes <osneil-drakes@hotmail.com>
2021-10-15 15:59:25 +0100

    write and pass two unit test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject { described_class.new(
+    first_name: "Harry",
+    last_name: "Styles",
+    email: "harry@example.com",
+    password: "password",
+    id: 1
+  )}
+
+  it "is valid with valid attributes" do 
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  #Still need to test for valid first_name, last_name and password
+
 end
