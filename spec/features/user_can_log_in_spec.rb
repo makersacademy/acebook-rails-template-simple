48773e12d9e6f2ab94074889f7cf98e2652a8552
Osneil Drakes <osneil-drakes@hotmail.com>
2021-10-15 15:34:56 +0100

    write and pass login feature tests

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -4,9 +4,9 @@ feature 'Login' do
   scenario "user can login" do
     visit('/')
     click_button "Login"
-    expect(page).to have_content("There is no log in fields")
     fill_in('email', with: 'test@example.com')
     fill_in('password', with: '12345678')
+    expect(page).to have_content("Login here!")
     click_button('Login')
     expect(page).to have_content("Hello, welcome to your feed")
   end 
