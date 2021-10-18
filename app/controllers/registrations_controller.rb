33c52d9824dca3729732513859a9b1f9e141a2d6
Florence Bain <florence_bain@outlook.com>
2021-10-18 17:15:13 +0100

    add image button, error using avatar

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,7 +17,8 @@ class RegistrationsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :avatar)
+    @user.avatar.attach(params[:avatar])
   end
 end 
 
