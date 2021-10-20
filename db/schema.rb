0c68f263a1acc9d00ffe53d1855a6a318c6483f4
Osneil Drakes <osneil-drakes@hotmail.com>
2021-10-20 15:39:36 +0100

    update index.html file for syntax error

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_10_18_150631) do
-
-ActiveRecord::Schema.define(version: 2021_10_18_152419) do
-
+ActiveRecord::Schema.define(version: 2021_10_19_094408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,5 +72,4 @@ ActiveRecord::Schema.define(version: 2021_10_18_152419) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
-end
 end
