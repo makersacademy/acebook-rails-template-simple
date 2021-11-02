602b768f37a2aeed6ca20b74a83042151e8b2e5a
Alex Wintle <alexwintle663@gmail.com>
2021-11-02 09:55:53 +0000

    feat: added authorization/authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authorized
+  helper_method :authorized
   helper_method :current_user
   helper_method :logged_in?
 
@@ -11,4 +13,9 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !current_user.nil?
   end
+
+  def authorized
+    redirect_to '/welcome' unless logged_in?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
-
+  get 'authorized', to: 'sessions#page_requires_login'
   resources :posts
 end
