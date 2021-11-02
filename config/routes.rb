513e756df9de159bbaf5deeb5616db25548813b8
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-02 08:53:35 +0000

    feat(*): add routes for logging in and welcome page, welcome page section next

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
 
   resources :posts
 end
