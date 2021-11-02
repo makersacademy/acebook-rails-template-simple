faba6f78ac9854f5168c68b691fa4aa0f4a497bb
Hassan Saleem <hassansaleem194@gmail.com>
2021-11-02 15:45:45 +0000

    started to write the login feature test

---
+++ spec/features/log_in_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature 'Log in', type: :feature do
+  scenario 'A user can log in to their Acebook account' do
+    visit('/welcome')
+    click_button('Login')
+    expect(page).to have_current_path('/login')
+    fill_in 'Username', with: 'user'
+    fill_in 'Password', with: '123'
+    click_button('Login')
+    expect(page).to have_current_path('/welcome')
+    expect(page).to have_content('You are Logged In, user')
+  end
+end
