8e8ca9e2c727f0e985ce2534fd6e28af8ad0f6a0
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-02 08:46:41 +0000

    feat(*): add users and sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
