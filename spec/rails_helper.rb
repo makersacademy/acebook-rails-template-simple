0da977aa5858d7bfd0421a3bf1515b968e4ea7b3
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-03 11:14:37 +0000

    feat(*): update all tests, exclude files that contain no functionality

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,13 @@ if ENV['RAILS_ENV'] == 'test'
   require 'simplecov'
   require 'simplecov-console'
   SimpleCov.start 'rails' do
+    # Following files ignored for now, because it is not clear what they're used for
+    add_filter 'app/mailers/application_mailer.rb'
+    add_filter 'app/channels/application_cable/connection.rb'
+    add_filter 'app/channels/application_cable/channel.rb'
+    add_filter 'app/jobs/application_job.rb'
+    
+    # Next line used to show the full coverage report in console
     SimpleCov.formatter = SimpleCov::Formatter::Console
   end
 end
