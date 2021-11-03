6723db25e4ed053409197b86f3c552f0b94efe0f
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-03 10:02:26 +0000

    test(sessions): write tests for #create

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+  describe "POST /" do
+    it "redirects to /login when user details are not confirmed" do
+      post :create, params: { username: 'user', password: 'passwordincorrect'} 
+      expect(response).to redirect_to(login_url)
+    end
+
+    it "redirects to /welcome when user details areconfirmed" do
+      User.create({username: 'user', password: 'password'})
+      post :create, params: { username: 'user', password: 'password'} 
+      expect(response).to redirect_to(welcome_url)
+    end
+  end
+end
