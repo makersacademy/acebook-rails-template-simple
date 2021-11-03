86e5a6249c077e21c6b45e7e953b4c05e517a9c4
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-03 13:10:02 +0000

    feat(*): add web_helper and dry all tests

--- spec/features/posts_display_by_newest_spec.rb
+++ spec/features/posts_display_by_newest_spec.rb
@@ -1,25 +1,12 @@
 require 'rails_helper'
+require 'web_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit('/welcome')
-    click_button('Sign up')
-    expect(page).to have_current_path('/users/new')
-    fill_in 'Username', with: 'user'
-    fill_in 'Password', with: 'password'
-    click_button('Create User')
-    expect(page).to have_current_path('/welcome')
-    expect(page).to have_content('You are Logged In, user')
+    sign_up
 
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "1: Hello, world!"
-    click_button "Submit"
-
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "2: Yellow, world!"
-    click_button "Submit"
+    add_new_post("1: Hello, world!")
+    add_new_post("2: Yellow, world!")
 
     expect(page).to have_content("2: Yellow, world!" ,"1: Hello, world!")
   end
