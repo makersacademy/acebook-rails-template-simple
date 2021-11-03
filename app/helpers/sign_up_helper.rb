f313a3e8db86afa77ef2d8a0d31d22e73e5ae11b
Hassan Saleem <hassansaleem194@gmail.com>
2021-11-03 09:22:55 +0000

    sign_up_spec test

--- app/helpers/sign_up_helper.rb
+++ app/helpers/sign_up_helper.rb
@@ -6,7 +6,5 @@ module SignUpHelper
     fill_in 'Username', with: 'user'
     fill_in 'Password', with: 'password'
     click_button('Create User')
-    expect(page).to have_current_path('/welcome')
-    expect(page).to have_content('You are Logged In, user')
   end
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,6 +3,9 @@ require 'sign_up_helper'
 
 RSpec.feature 'Sign up', type: :feature do
   scenario 'A user can sign up to Acebook' do
-    expect.(page.sign_up).to Equal
+    # include ActionView::Helpers
+    sign_up
+    expect(page).to have_current_path('/welcome')
+    expect(page).to have_content('You are Logged In, user')
   end
 end
