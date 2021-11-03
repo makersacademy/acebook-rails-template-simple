9e13c62bce70cfcc328e670ce981060482b90c43
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-03 16:24:08 +0000

    refactor(*): tidy code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,6 @@ class UsersController < ApplicationController
   def create
     @user = User.create(params.require(:user).permit(:username, :password))
     session[:user_id] = @user.id
-    redirect_to '/posts'
+    redirect_to posts_url
   end
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -5,7 +5,6 @@ RSpec.feature 'Sign up', type: :feature do
     sign_up
 
     expect(page).to have_current_path('/posts')
-    # uncomment next line when flash message has been implemented
-    # expect(page).to have_content('You are Logged In, user')
+    expect(page).to have_content('user')
   end
 end
