8ac52237c1e4df02699b27e67899bdb5589f7d1c
Hassan Saleem <hassansaleem194@gmail.com>
2021-11-03 11:40:12 +0000

    added change to sign out spec

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -12,5 +12,6 @@ RSpec.feature 'Sign out', type: :feature do
     expect(page).to have_content('You are Logged In, user')
     click_button('Sign out')
     expect(page).to have_current_path('/welcome')
+    expect(page).to_not have_content('You are Logged In, user')
   end
 end
