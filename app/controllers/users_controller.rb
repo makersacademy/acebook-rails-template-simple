95af73a224efab2f54fcd9c3ee3170fd9c850730
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-03 15:37:28 +0000

    feat(*): fix controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,6 @@ class UsersController < ApplicationController
   def create
     @user = User.create(params.require(:user).permit(:username, :password))
     session[:user_id] = @user.id
-    redirect_to '/welcome'
+    redirect_to '/posts'
   end
 end
