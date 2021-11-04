3840c2593f4a253e1909a00608880cf8bb6c5dec
Alex Wintle <alexwintle663@gmail.com>
2021-11-04 17:58:13 +0000

    refactor(view_specific_users_post_spec): changed 1: Hello, world! to, user1: Hello, world! (2: became => user2:)

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe PostsController, type: :controller do
     it "gets a specific post when passed the post id" do
       post :create, params: { post: { message: "Hello, world!" } }
       # get the post id
-      puts post_id = Post.all.first.id
+      post_id = Post.all.first.id
 
       get :show, params: { id: post_id }
       expect(response).to have_http_status(200)
@@ -43,22 +43,11 @@ RSpec.describe PostsController, type: :controller do
       post :create, params: { post: { message: "1: Hello, world!" } }
       post :create, params: { post: { message: "2: Yellow, world!" } }
 
-      puts user_id = Post.all.first.user_id
-
-      get :showSpecificUsersPost, params: { user_id: user_id }
-      expect(response).to have_http_status(200)
-    end
-    end
-
-  describe "GET /:user_id/posts" do
-    it "gets a specific post when passed the post id" do
-      post :create, params: { post: { message: "1: Hello, world!" } }
-      post :create, params: { post: { message: "2: Yellow, world!" } }
-
-      puts user_id = Post.all.first.user_id
+      user_id = Post.all.first.user_id
 
       get :showSpecificUsersPost, params: { user_id: user_id }
       expect(response).to have_http_status(200)
     end
   end
+
 end
--- spec/features/view_specific_users_post_spec.rb
+++ spec/features/view_specific_users_post_spec.rb
@@ -3,26 +3,26 @@ require 'rails_helper'
 RSpec.feature "Specific Users' Posts", type: :feature do
   before(:each) do
     sign_up
-    add_new_post("1: Hello, world!")
+    add_new_post("user1: Hello, world!")
     visit('/posts')
     click_link('Sign out')
 
     sign_up_different_user
     visit('/posts')
-    add_new_post("2: Yellow, world!")
+    add_new_post("user2: Yellow, world!")
   end
 
   scenario "Can submit post and navigate directly to post by post_id" do
-    user_id = Post.find_by(message: '1: Hello, world!').user_id
+    user_id = Post.find_by(message: 'user1: Hello, world!').user_id
     visit("/posts/#{user_id}/posts")
 
-    expect(page).to have_content('1: Hello, world!')
-    expect(page).to_not have_content('2: Yellow, world!')
+    expect(page).to have_content('user1: Hello, world!')
+    expect(page).to_not have_content('user2: Yellow, world!')
 
-    user_id_two = Post.find_by(message: '2: Yellow, world!').user_id
+    user_id_two = Post.find_by(message: 'user2: Yellow, world!').user_id
     visit("/posts/#{user_id_two}/posts")
 
-    expect(page).to_not have_content('1: Hello, world!')
-    expect(page).to have_content('2: Yellow, world!')
+    expect(page).to_not have_content('user1: Hello, world!')
+    expect(page).to have_content('user2: Yellow, world!')
   end
 end
