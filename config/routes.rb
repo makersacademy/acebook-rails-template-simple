4cc98407c5d9b49f049e79bcc2148740d204fe86
Stella Martin <stellamartin222@gmail.com>
2021-11-05 15:32:39 +0000

    comments form now renders on the page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   resources :posts
-  resources :articles do
+  resources :post do
     resources :comments
   end
 end
