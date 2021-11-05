96fa398469591601c258bb9aa672330f8f8a9f68
michelle-watkins <87414288+michelle-watkins@users.noreply.github.com>
2021-11-05 11:29:57 +0000

    Revert "Feature/likes model and database"

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'likes/update'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
-  put '/post/:id/like', to: 'posts#like', as: 'like'
   
   resources :users, only: [:new, :create]
   resources :posts
