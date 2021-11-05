80ad4aca3742ef6f8e4ae239592db2eab440613f
Alex Wintle <alexwintle663@gmail.com>
2021-11-05 14:47:34 +0000

    docs/tests: updated changelog and created unit test

--- spec/features/view_specific_users_post_spec.rb
+++ spec/features/view_specific_users_post_spec.rb
@@ -12,7 +12,7 @@ RSpec.feature "Specific Users' Posts", type: :feature do
     add_new_post("user2: Yellow, world!")
   end
 
-  scenario "Can submit post and navigate directly to post by post_id" do
+  scenario "Visit a specific users page, which displays their posts" do
     user_id = Post.find_by(message: 'user1: Hello, world!').user_id
     visit("/posts/#{user_id}/posts")
 
@@ -24,5 +24,20 @@ RSpec.feature "Specific Users' Posts", type: :feature do
 
     expect(page).to_not have_content('user1: Hello, world!')
     expect(page).to have_content('user2: Yellow, world!')
+    end
+
+  scenario "Can navigate to any post by clicking on their username" do
+    user_id = Post.find_by(message: 'user1: Hello, world!').user_id
+    username = User.find_by(id: user_id).username
+
+    visit("/posts")
+    expect(page).to have_content('user1: Hello, world!')
+    expect(page).to have_content('user2: Yellow, world!')
+
+    click_link(username)
+    expect(page).to have_current_path("/posts/#{user_id}/posts")
+    expect(page).to have_content('user1: Hello, world!')
+    expect(page).to_not have_content('user2: Yellow, world!')
   end
+
 end
