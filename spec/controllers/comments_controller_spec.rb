1711e2178edafd0c781e6789396fd765a37f68c6
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-08 12:26:23 +0000

    test(comments_controller): test that comments are created

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -5,16 +5,19 @@ RSpec.describe CommentsController, type: :controller do
     allow(controller).to receive(:logged_in?) { true }
     user = User.create(username: "user", password: "password", id: 1)  
     allow(controller).to receive(:current_user) { user }
-    posts = Post.create(message: "This is a post", id: 1)
+    posts = Post.create(message: "This is a post", id: 1, user_id: 1)
   end
 
-  describe "POST /posts" do
+  describe "Creating a comment" do
     it "responds with 200" do
       post :create, params: { comment: { body: "Hello, world!" }, post_id: 1 }
       expect(response).to redirect_to("/posts/1")
     end
+
+    it "creates a comment" do
+      post :create, params: { comment: { body: "Hello, world!" }, post_id: 1 }
+      expect(Comment.find_by(body: "Hello, world!")).to be
+      expect(Comment.find_by(post_id: 1)).to be
+    end
   end
-
-
-
 end
