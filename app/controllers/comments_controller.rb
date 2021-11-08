e6a4677587588d6f83f791a210625679e7b19d8e
Stella Martin <stellamartin222@gmail.com>
2021-11-08 09:40:00 +0000

    completed comments functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.post_id = params[:post_id]
+    @comment.user_id = session[:user_id]
   
     @comment.save
   
--- db/migrate/20211105122700_create_comments.rb
+++ db/migrate/20211105122700_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.belongs_to :user, null: false, foreign_key: true
-      t.text :body
+      t.string :body
       t.belongs_to :post, null: false, foreign_key: true
 
       t.timestamps
