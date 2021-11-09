a23d8e1284bf0263faada2ffc49ff3369610f1c0
Hassan Saleem <hassansaleem194@gmail.com>
2021-11-09 16:15:46 +0000

    feature test for timestamps

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,22 +5,14 @@ module ApplicationHelper
 
   end
 
-  def lessthanaday?(date)
+  def less_than_a_day?(date)
     new_date = Time.parse(date.to_s)
     current_time = Time.now
-    current_time - new_date > -86400
+    current_time - new_date <86400
   end
 
-  def showtime(date)
-    
+  def show_time(date)
+    less_than_a_day?(date) ? "#{distance_of_time_in_words(Time.now, date)} ago" : convert_date(date)
   end
-
-
 
 end
-
-
-# new method, pass the date (same as line 3, date object) and see if it lessthanaday?
-# time.now gets current time, try to remove the date object (time deltas)
-# if less than a day return true if > return false
-# call the method and pass the string, if true show time if false show date
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -7,19 +7,30 @@ describe ApplicationHelper do
     end
   end
 
-  describe '#lessthanaday?' do
+  describe '#less_than_a_day?' do
     it 'checks if post was made <24 hours ago' do
-      allow(Time).to receive(:now).and_return(Time.new(2021, 11, 9, 14, 0, 0))
-      expect(helper.lessthanaday?('2021-11-09 15:08:06 UTC')).to eq(true)
+      allow(Time).to receive(:now).and_return(Time.new(2021, 11, 9, 15, 0, 0))
+      expect(helper.less_than_a_day?('2021-11-09 14:08:06 UTC')).to eq(true)
     end
 
     it 'checks if post was made >24 hours ago' do
-      allow(Time).to receive(:now).and_return(Time.new(2021, 11, 8, 15, 0, 0))
-      expect(helper.lessthanaday?('2021-11-09 15:08:06 UTC')).to eq(false)
+      allow(Time).to receive(:now).and_return(Time.new(2021, 11, 9, 15, 0, 0))
+      expect(helper.less_than_a_day?('2021-11-06 15:08:06 UTC')).to eq(false)
     end
   end
+
+  describe '#show_time' do
+    it 'returns the time at which a post was made in words for posts made <24 hours ago' do
+      allow(Time).to receive(:now).and_return(Time.new(2021, 11, 8, 15, 0, 0))
+      expect(show_time('2021-11-08 14:00:00 UTC')).to eq('about 1 hour ago')
+    end
+
+    it 'returns the time at which a post was made in dd/mm/yyyy format for posts made >24 hours ago' do
+      allow(Time).to receive(:now).and_return(Time.new(2021, 11, 8, 15, 0, 0))
+      expect(show_time('2021-11-07 13:00:00 UTC')).to eq('07-11-2021')
+    end
+  end
+
+
   
-
-
-
 end
