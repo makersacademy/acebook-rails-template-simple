5de6672e5af1cc3a6b780181820e2212e586650d
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-09 16:57:09 +0000

    docs(changelog): update changelog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Use AWS S3 for storing images in production
   config.active_storage.service = :amazon
 end
