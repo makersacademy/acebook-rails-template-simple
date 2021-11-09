c855543710cf9f39eb2b66f703cfbf8440683923
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-11-09 16:25:01 +0000

    Skipped verify authenticity token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def create
-    @user = User.create(params[:first_name], params[:last_name], params[:email], params[:password])
+    @user = User.create(params[:first_name], params[:last_name], params[:email], params[:picture], params[:password])
   end
 end
