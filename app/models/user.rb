127887716707b5298e5eb784ed07f44b9396d0f9
Arthur Fincham <arthurfincham@me.com>
2021-11-10 14:47:32 +0000

    add shrine and bcrypt gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
+include ImageUploader::Attachment(:image)
+has_secure_password
   has_many :posts
-  
+validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Invalid email" }
 end
