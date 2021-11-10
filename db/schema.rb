a5ce0cf791163a663e1099db95efe8fb240047ed
Emmanuel Folorunsho <emmanuelfolorunsho@icloud.com>
2021-11-10 11:53:30 +0000

    adding user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_05_26_114520) do
+ActiveRecord::Schema.define(version: 2021_11_10_113954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 2017_05_26_114520) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", limit: 30
+    t.string "last_name", limit: 30
+    t.date "date_of_birth"
+    t.string "email", limit: 50
+    t.string "password_digest"
+    t.string "image_data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
