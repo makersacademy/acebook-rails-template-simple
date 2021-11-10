1e8838dafe87af7f862fb618e4e2a1b75e9ac3f6
Arthur Fincham <arthurfincham@me.com>
2021-11-10 14:49:06 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- get 'sign_up', to: 'registrations#new'
-  post 'sign_up', to: 'registrations#create'
-    get 'sign_in', to: 'sessions#new'
-  post 'sign_in', to: 'sessions#create', as: 'log_in'
-  delete 'logout', to: 'sessions#destroy'
+  root to: "welcome#index"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create", as: 'log_in'
+  delete "logout", to: "sessions#destroy"
   resources :posts
 end
