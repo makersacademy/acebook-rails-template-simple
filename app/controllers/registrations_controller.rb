cdffa1093838781580f4a8876a029aa600c93fba
Arthur Fincham <arthurfincham@me.com>
2021-11-10 14:48:01 +0000

    update registration controller methods

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,18 @@
 class RegistrationsController < ApplicationController
+
   def new
-    
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(params.require(:user).permit(:username, :password, :email, :password_confirmation, :image, :first_name, :last_name))
+
+    session[:user_id] = @user.id
+    if @user.save
+  session[:user_id] = @user.id
+  redirect_to root_path, notice: "Account created!"
+else
+  render :new
+end
   end
 end
