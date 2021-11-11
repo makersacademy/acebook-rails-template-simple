e04ecbea95da4e947535252b0e899cf1168fea44
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-11-11 16:22:09 +0100

    add user.save and user.new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,22 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
-    @user = User.create(
+    @user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       picture: params[:picture],
       password: params[:password]
     )
-    # strict inputs e.g. max length
-    session[:user_id] = @user.id
+    if @user.save
+      flash[:notice] = 'User saved successfully'
+        # strict inputs e.g. max length
+      session[:user_id] = @user.id
+      redirect_to posts_url
 
-    redirect_to posts_url
+    else
+      flash[:alert] = 'User was not saved.'
+      render :new
+    end
   end
-
 end
