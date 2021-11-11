c61a28c2fee60afc3bd86ae251938492286e1e6e
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-11 12:00:33 +0000

    refactor(show_specific_user_posts): rename file into snake case

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get '/posts/search', to: 'posts#search_post'
   get '/posts/search/:search', to: 'posts#search', as: 'search'
   put '/posts/:id/like', to: 'posts#like', as: 'like'
-  get 'posts/:user_id/posts', to: 'posts#showSpecificUsersPost'
+  get 'posts/:user_id/posts', to: 'posts#show_specific_users_post'
   post '/posts/:id/comment', to: 'comments#create', as: 'comment'
 
   resources :users, only: [:new, :create]
