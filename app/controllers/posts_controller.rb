be4d4dd224f67d6208cdfffefa2b070a5fcb82c7
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-11 09:13:16 +0000

    feat(*): routing hides params in url

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,6 +42,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def search_post
+    redirect_to("/posts/search/#{params[:search]}")
+  end
+
   private
 
   def post_params
--- spec/features/search_functionality_spec.rb
+++ spec/features/search_functionality_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Search", type: :feature do
-  
+  scenario "Redirects to the search page when a search string passed" do
+    sign_up
+    add_new_post('Hello world')
+
+    fill_in :search, with: 'Hello'
+    
+    expect(page).to have_current_path('/')
   end
 end
