7852f6c847181fd1a28a5f03c706a0f65109ee30
Jacob Cho <jacobcho@live.com>
2021-11-11 13:16:45 +0000

    logout button

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   post "sign_up", to: "registrations#create"
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create", as: 'log_in'
-  delete "logout", to: "sessions#destroy"
+  get "logout", to: "sessions#destroy"
   resources :posts
 end
