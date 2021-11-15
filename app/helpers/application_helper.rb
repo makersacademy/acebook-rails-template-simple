6aaf6dfee897c5fde44b33f75597e4f3e119a007
Arthur Fincham <arthurfincham@me.com>
2021-11-15 16:22:54 +0000

    post view and form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,14 @@ module ApplicationHelper
     now = Time.now.utc.to_date
     now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
   end
+
+  def id2name(user_id)
+    user = User.find(user_id)
+    user.first_name + "  " + user.last_name
+  end
+
+  def id2avatar(user_id)
+    user = User.find(user_id)
+    user.image_url
+  end
 end
