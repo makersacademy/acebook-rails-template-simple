5bc707d067ae17249c1e77bc04c0d028868495a0
Arthur Fincham <arthurfincham@me.com>
2021-11-16 14:16:57 +0000

    remove public uploads from git

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_115710) do
-
+ActiveRecord::Schema.define(version: 2021_11_15_151033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
