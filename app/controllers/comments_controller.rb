de9de7ad7be379a0660b4a416988cb92d7656282
Emmanuel Folorunsho <emmanuelfolorunsho@icloud.com>
2021-11-17 15:37:20 +0000

    commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController 
 
   def create 
-    @comment = Current.user.comments.new(post_params)
-    if @comment.save
+  comment = Current.user.comments.new(post_params)
+    if comment.save
       flash[:notice] = 'Comment was successfully created.'
-      redirect_to root_path
+      #redirect_to root_path
     else
       flash[:notice] = 'There was a problem.'
     end
