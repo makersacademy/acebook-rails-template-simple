09d82ead39122bf217e9a17e74f1aede921891e4
Jacob Cho <jacobcho@live.com>
2021-11-19 13:09:25 +0000

    friend suggestion panel

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,16 @@
 class WelcomeController < ApplicationController
   def index
+    # The welcome index view page renders many other pages (posts, profile etc)
+    # The variables requires from those pages need to be defined here
+
     @posts = Post.order("created_at DESC")
     @post = Post.new
     @comment = Comment.new 
     @likes = Like
+
+    @user = Current.user
+    @users = User.all
+
   end
 
 end
