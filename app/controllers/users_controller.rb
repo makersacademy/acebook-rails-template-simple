c68d3ba3d9e9e6331ef07bab2909e0a0e1c0ae9a
Arthur Fincham <arthurfincham@me.com>
2021-11-19 12:20:12 +0000

    add user posts to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def profile
     @user = Current.user
+    @posts = Current.user.posts.order("created_at DESC")
+    @comment = Comment.new
+@likes = Like
   end
 
   def everyone
