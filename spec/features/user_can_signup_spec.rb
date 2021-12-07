5b371227735f9689e26cb5954a8a494daf168cd7
Will <w_birdseye@hotmail.co.uk>
2021-12-07 15:43:45 +0000

    User class created

--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "Sign up", type: :feature do
-#   scenario "Can sign up for account" do
-#     visit "/"
-#     click_link "Sign up"
-#     fill_in "Email", with: "user@email.com"
-#     fill_in "Password", with: "password"
-#     fill_in "Confirm Password", with: "password"
-#     click_button "Create account"
-#     expect(page).to have_content("Login")
-#   end
+  scenario "There is a sign up form on page" do
+    visit "/"
+    click_link "this is the link to go to the sign up page"
+    fill_in "Email", with: "user@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirm Password", with: "password"
+    click_button "Create account"
+    expect(page).to have_content("Login")
+  end
 
 scenario "can visit sign up page" do
    visit "/"
