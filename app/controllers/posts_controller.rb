457318d7daa4bc76cc466300d69163319b8a9b5e
Mohammed Salah <mohammed_salah@icloud.com>
2021-12-08 10:29:36 +0000

    rerouted form for single page posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-  def new
-    @post = Post.new
-  end
+  #def new
+   # @post = Post.new
+  #end
 
   def create
     @post = Post.create(post_params)
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @post = Post.new
   end
 
   private
