787069b62f5189b06e551b3d93e505c36bf64660
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-12-09 11:49:26 +0100

    Add image in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  
   root to: 'main#index'
 
   get 'sign_up', to:'registrations#new' 
   post 'sign_up', to:'registrations#create' 
   
   resources :posts
-
+  resources :post_images
 end
