5c6b386681b6b70680c36b536197577e97589406
Will <w_birdseye@hotmail.co.uk>
2021-12-10 12:05:50 +0000

    access to posts page denied without login plus test

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit "/posts"
+    visit "/"
+    click_link "Haven't got an account? Sign up!"
+    fill_in "user[email]", with: "user@email.com"
+    fill_in "user[password]", with: "password"
+    fill_in "user[password_confirmation]", with: "password"
+    click_button "Create User"
+    fill_in "email", with: "user@email.com"
+    fill_in "password", with: "password"
+    click_button "Login"
     # click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
