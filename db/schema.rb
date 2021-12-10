a47dbf258cac4f2fa89ee11116d8706ea7fd7c73
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-12-10 14:17:56 +0000

    restore test changes and merge main

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2021_12_09_103313) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "post_images", force: :cascade do |t|
     t.string "title"
     t.text "content"
