d75b53742903ca36403ad40ba5a0b895da00030d
Myoung <mhbae93@gmail.com>
2021-12-10 15:11:45 +0000

    feature for deleting each comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,16 @@ class CommentsController < ApplicationController
     redirect_to post_image_path(@post_image)
   end
 
+  def destroy
+    @post_image = PostImage.find(params[:post_image_id])
+    @comment = @post_image.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_image_path(@post_image)
+  end
+
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
