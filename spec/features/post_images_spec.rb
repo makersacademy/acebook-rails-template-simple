675a09ba12cf04f965dea54c32a56b99ccda5843
Myoung <mhbae93@gmail.com>
2021-12-13 12:06:24 +0000

    feature test passing for checking time post and comments were made

--- spec/features/post_images_spec.rb
+++ spec/features/post_images_spec.rb
@@ -97,4 +97,28 @@ RSpec.feature 'Post Image', type: :feature do
     click_link 'Back'
     expect(page).to have_content('Number of comments: 0')
   end
+
+  scenario 'you can see when an Image was created' do
+    time = Time.now.strftime('%A %d %B %Y, %I:%M%p')
+    visit '/post_images'
+    click_link 'New Post Image'
+    fill_in 'post_image[title]', with: 'Hello Cat'
+    fill_in 'post_image[content]', with: 'This is a photo of an evil cat'
+    click_button 'Create Post image'
+    click_link 'Back'
+    expect(page).to have_content("Timestamp: #{time}")
+  end
+
+  scenario 'you can see when a comment was made' do
+    time = Time.now.strftime('%A %d %B %Y, %I:%M%p')
+    visit '/post_images'
+    click_link 'New Post Image'
+    fill_in 'post_image[title]', with: 'Hello Cat'
+    fill_in 'post_image[content]', with: 'This is a photo of an evil cat'
+    click_button 'Create Post image'
+    fill_in 'comment[commenter]', with: 'Test comment'
+    fill_in 'comment[body]', with: 'this is the first comment'
+    click_button 'Create Comment'
+    expect(page).to have_content("Timestamp: #{time}")
+  end
 end
