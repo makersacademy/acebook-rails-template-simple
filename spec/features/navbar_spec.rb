8ffc8f7934f8d4135f6a8eb19ae785da98ec3920
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-12-17 10:58:33 +0000

    fix acebook link test

--- spec/features/navbar_spec.rb
+++ spec/features/navbar_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Navbar", type: :feature do
   scenario "Navbar to not have content without login" do
     visit "/"
     @nav = find(".navbar")
-    expect(@nav).to have_link("Acebook")
+    expect(@nav).to have_css(".acebook_logo")
     expect(@nav).to_not have_button("Logout")
     
   end
