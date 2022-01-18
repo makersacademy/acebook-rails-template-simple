8af59eb6350f7f13ee5eca04075a3c6498eb785e
Fadaka <master_kunle14@hotmail.co.uk>
2022-01-18 16:44:17 +0000

    passed all test and added subject mock for our user validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,30 @@
 require 'rails_helper'
 
+
+
 RSpec.describe User, type: :model do
+  subject {
+    User.new(name: "Michael",
+             email: "Michael@gmail.com",
+            created_at: DateTime.now,
+            updated_at: DateTime.now,
+            id: 1)
+    }
   it "is valid with valid attributes" do
-    expect(User.new).to be_valid
+    expect(subject).to be_valid
   end
 
   it "is not valid without a name"
   it "is not valid without a email"
 
   it "is not valid without a name" do
-    user = User.new(name: nil)
-    expect(user).to_not be_valid
+    user_2 = User.new(name: nil, email:"email")
+    expect(user_2).to_not be_valid
   end
 
   it "is not valid without a email" do
-    user = User.new(email: nil)
-    expect(user).to_not be_valid
+    user_1 = User.new(name:"person", email: nil)
+    expect(user_1).to_not be_valid
   end
 
 end
