22a72cda121ee962ea6ab1d42a5b654cb476717c
kehinde alaka <kennyalaka93@gmail.com>
2022-01-18 16:43:50 +0000

    set root page to post/index

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -24,6 +24,8 @@ RSpec.describe PostsController, type: :controller do
     it "responds with 200" do
       get :index
       expect(response).to have_http_status(200)
-    end
+    end   
   end
+
+
 end
