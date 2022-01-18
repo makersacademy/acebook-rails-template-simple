a2f428c2e9f5b8ad6481cf48f598acaf763bf645
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-01-18 14:53:21 +0000

    can log in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
 
+  # get 'new_user_registration'
+  # get 'new_user_session'
   resources :posts
 end
