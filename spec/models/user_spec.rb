fd9109537b732a657b40f89332ea74161fd46aa7
Fadaka <master_kunle14@hotmail.co.uk>
2022-01-18 16:14:11 +0000

    added test for user , including validation for username and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(User.new).to be_valid
+  end
+
+  it "is not valid without a name"
+  it "is not valid without a email"
+
+  it "is not valid without a name" do
+    user = User.new(name: nil)
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without a email" do
+    user = User.new(email: nil)
+    expect(user).to_not be_valid
+  end
+
 end
