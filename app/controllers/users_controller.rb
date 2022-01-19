d219fba17e8f83785545b6cb9ba210695727ef1e
Inimesh <ijmeshoulam@gmail.com>
2022-01-19 17:01:05 +0000

    debugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
   end
+
+  def show 
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    p params
+    @user = User.new(user_params)
+
+    if @user.save
+      p "hello"
+      redirect_to posts_path
+      p "I am run"
+    else
+      p "this else is being run"
+      render :new
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get "/", to: "users#index"
-  get "/signup", to: "signup#index"
+  # get "/", to: "users#index"
+  # get "/signup", to: "signup#index"
+
+  root "users#index"
+
+  resources :users
 
   resources :posts do
     resources :comments
