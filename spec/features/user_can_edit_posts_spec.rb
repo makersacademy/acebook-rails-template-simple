a7ef6d04852f722301efe12b1c51a6780f39d397
kehinde alaka <kennyalaka93@gmail.com>
2022-01-19 11:39:04 +0000

    feature text for edit button

---
+++ spec/features/user_can_edit_posts_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "Timeline", type: :feature do
+  scenario "Can edit posts and view changes" do
+    visit "/"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_button "Edit"
+    fill_in "Message", with: "Hello, world again"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world again")
+  end
+end
