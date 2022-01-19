e168434f35e08d05e6e23b6bea477720b3610d27
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-01-19 11:26:54 +0000

    update

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "Sign up", type: :feature do
+  sign_up_email = '//*[@id="user_email"]'
+  sign_up_password = '//*[@id="user_password"]'
+  sign_up_password_confirmation = '//*[@id="user_password_confirmation"]'
+  sign_up_button_confirmation = "/html/body/form/div[4]/input"
+  sign_up_button = "/html/body/button[1]/a"
+
   scenario "Can sign up" do
     visit "/"
-    sign_up_button = "/html/body/button[1]/a"
     find(:xpath, sign_up_button).click()
-
-    sign_up_email = '//*[@id="user_email"]'
-    sign_up_password = '//*[@id="user_password"]'
-    sign_up_password_confirmation = '//*[@id="user_password_confirmation"]'
-    sign_up_button_confirmation = "/html/body/form/div[4]/input"
-
     find(:xpath,sign_up_email).set("test@test.com")
     find(:xpath,sign_up_password).set("123456")
     find(:xpath,sign_up_password_confirmation).set("123456")
@@ -18,14 +17,22 @@ RSpec.feature "Sign up", type: :feature do
     expect(page).to have_content("You are signed in as test@test.com")
     expect(page).not_to have_content "Sign up"
   end
+
+  scenario "Cannot sign up if password is too short" do
+  end
+
+  scenario "Cannot sign up if email is invalid" do
+    # Add a notice that says email is invalid
+  end
+
+  scenario "Cannot sign up if email is taken" do
+  end
+
+  scenario "Cannot sign up if passwords don't match" do
+  end
+
+
+
 end
-
-# email = //*[@id="user_email"]
-
-# password = //*[@id="user_password"]
-
-# password_confirm = //*[@id="user_password_confirmation"]
-
-# sign_up_button_last = /html/body/form/div[4]/input
 
 
