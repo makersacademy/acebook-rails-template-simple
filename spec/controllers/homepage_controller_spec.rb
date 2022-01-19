8e428e7425959165edb77071fe941bcdc4fd6fa4
Chris Clement <chrisclement@Chriss-Air.connect>
2022-01-19 15:39:14 +0000

    user_can_log_in feature test now passing

--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -3,23 +3,13 @@ require 'rails_helper'
 RSpec.describe HomepageController, type: :controller do
   describe "POST /" do
     it "responds with 200" do
-      p "we are here"
+      # users is not recognised. We are trying to reference the spec/features/users.yml file to input
+      # a user into a test database. Work in Progress.
       p users(:example_user).email
       post :create, params: { email: "dagswebb@gmail.com", password: 'hello' } 
       expect(response).to redirect_to('/posts')
     end
 
-#     it "creates a User when all required parameters are filled" do
-#       post :create, params: { name: "Chris", email: "chris@gmail.com", password: '123' }
-#       p "This is the test where all params are filled"
-#       p User.find_by(email: "chris@gmail.com")
-#       expect(User.find_by(email: "chris@gmail.com").name).to eq "Chris"
-#       expect(User.find_by(email: "chris@gmail.com").email).to eq "chris@gmail.com"
-#     end
-
-#     it "does NOT create a User if any required parameters are NOT filled" do
-#       expect{post :create, params: { email: "chris@gmail.com", password: '123' }}.to raise_error
-#     end
    end
 
   describe "GET /" do
--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "Log in", type: :feature do
   scenario "Can Log in on homepage" do
+    visit "/signup"
+    fill_in "name", with: "Chris"
+    fill_in "email", with: "chris@gmail.com"
+    fill_in "password", with: "123"
+    click_button "Sign Up"
     visit "/homepage"
     fill_in "email", with: "chris@gmail.com"
     fill_in "password", with: "123"
     click_button "Log in"
-    expect(page).to have_content("Welcome Chris!")
+    expect(page).to have_content("New post")
   end
 end
