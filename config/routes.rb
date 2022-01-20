1e5bf3cc972c5a9943af7e64ace4cd620a1eb67b
j-cort <81960011+j-cort@users.noreply.github.com>
2022-01-20 10:16:59 +0000

    add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get "/", to: "users#index"
+  root "home#index"
   get "/signup", to: "signup#index"
 
   resources :posts do
