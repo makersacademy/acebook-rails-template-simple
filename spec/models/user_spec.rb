4dc61de1919d899da682bca412818d8844b3aba3
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-01-20 11:17:03 +0000

    added unit test for adding avatar image to the user class

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "upload avatar" do
+    before do
+      @user = User.create(id: 1, email: "test")
+      @user.avatar.attach(io: File.open("lib/assets/Scooby-Doo_test_picture.png"), 
+      filename: "Scooby-Doo_test_picture.png", content_type: "image/png")
+    end 
+
+    it "Img added to user model" do
+      expect(@user.avatar.attached?).to be true
+      expect(@user.avatar.filename).to eq("Scooby-Doo_test_picture.png")
+    end
+
+    it "Img replaced when another is attached " do
+      @user.avatar.attach(io: File.open("lib/assets/ShaggyRogers_test_picture.png"), 
+      filename: "ShaggyRogers_test_picture.png", content_type: "image/png"
+
+      expect(@user.avatar.attached?).to be true
+      expect(@user.avatar.filename).to eq("ShaggyRogers_test_picture.png")
+    end
+  end
 end
