0d0c7fc6f969809292e3f547ae6f704595bfb685
Chris Clement <chrisclement@Chriss-Air.connect>
2022-01-20 16:38:22 +0000

    Log in page now tells user if password is incorrect or user is not found

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,7 +4,11 @@ class HomepageController < ApplicationController
         @user = User.find_by(email: signup_params[0], password: signup_params[1])
         if @user == nil
             redirect_to "/homepage"
-            flash.alert = "User not found, please try again"
+            if User.find_by(email: signup_params[0]) == nil
+                flash.alert = "User not found, please try again"
+            else
+                flash.alert = "Incorrect Password, please try again"
+            end
         else
             session[:current_user_id] = @user.id
             p "Who is stored in the session"
