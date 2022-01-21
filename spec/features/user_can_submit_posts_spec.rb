b4f0e9ab27a68e5673324edd2f65dca5915dc353
bryn9696 <bryn2009@live.co.uk>
2022-01-21 12:21:21 +0000

    can upload just a picture and just text

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,10 +1,29 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature "Timeline", type: :feature do
-#   scenario "Can submit posts and view them" do
-#     visit "/"
-#     fill_in "Message", with: "Hello, world!"
-#     click_button "Submit"
-#     expect(page).to have_content("Hello, world!")
-#   end
-# end
+RSpec.feature "Timeline", type: :feature do
+  scenario "Can submit posts and view them" do
+    sign_up
+    visit "/posts"
+    find(:xpath, '//*[@id="post_message"]').set("Hello, world!")
+    find(:xpath, "/html/body/form[1]/div/input").click
+    expect(page).to have_content("Hello, world!")
+  end
+
+  scenario "Can submit picture and text" do
+    sign_up
+    visit "/posts"
+    find(:xpath, "/html/body/form[1]/input[4]").set("lib/assets/Scooby-Doo_test_picture.png")
+    find(:xpath, "/html/body/form[1]/input[3]").set("Test")
+    find(:xpath, "/html/body/form[1]/div/input").click
+    expect(page).to have_content("Test")
+  end
+
+  scenario "Can submit just a picture as a posts" do
+    sign_up
+    visit "/posts"
+    find(:xpath, "/html/body/form[1]/input[4]").set("lib/assets/Scooby-Doo_test_picture.png")
+    find(:xpath, "/html/body/form[1]/div/input").click
+    expect(page).to 
+  end
+
+end
