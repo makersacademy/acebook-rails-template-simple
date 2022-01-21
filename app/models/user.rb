d54f46f84a003b350e878de08264bc0b9d7a0832
tc1316 <tituschang@hotmail.co.uk>
2022-01-21 12:45:26 +0000

    Fixed tests for sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, uniqueness: true
+  validates :username, uniqueness: true, presence: true
   has_one_attached :avatar
 end
