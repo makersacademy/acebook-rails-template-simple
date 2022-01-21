f8366029b9ad37fb9d4dd48b14a4c935310ac9bd
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-01-21 16:13:08 +0000

    a user can only like a post once added

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,8 +4,23 @@ class LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @post.likes.create(user_id: current_user.id)
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
+    else
+      @post.likes.create(user_id: current_user.id)
+    end
     redirect_to posts_path(@post)
+  end
+
+
+  # @post.likes.create(user_id: current_user.id)
+  #   redirect_to posts_path(@post)
+
+  private 
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id:
+    params[:post_id]).exists?
   end
 
 
