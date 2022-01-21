c90eded893c7e41b820aff86032241b20a739389
Chris Clement <chrisclement@Chriss-Air.connect>
2022-01-21 14:41:09 +0000

    Feature tests now passing for logging in, signing up and creating a text post

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
+    visit "/signup"
+    fill_in "name", with: "Chris"
+    fill_in "email", with: "chris@gmail.com"
+    fill_in "password", with: "1234567"
+    attach_file("profile_pic", File.join(Rails.root, '/app/assets/images/spiderman.jpeg'))
+    click_button "Sign Up"
+    visit "/homepage"
+    fill_in "email", with: "chris@gmail.com"
+    fill_in "password", with: "1234567"
+    click_button "Log in" 
     visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
+    click_link "Create new post"
+    fill_in "post_content", with: "Hello, world!"
+    click_button "Create Post"
     expect(page).to have_content("Hello, world!")
   end
 end
