a80af10c24e73e766ace867e40dd507f20c85fd4
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-01-21 11:55:02 +0000

    users can only delete their own posts

--- spec/features/post/user_can_delete_post_spec.rb
+++ spec/features/post/user_can_delete_post_spec.rb
@@ -2,8 +2,26 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can delete a post" do
+    user_sign_up
     create_post
     click_link "Delete post"
     expect(page).not_to have_content("Hello, world!")
+  end
+
+  scenario "Users can only delete a post they made themselves (from timeline)" do
+    user_sign_up
+    create_post
+    log_out
+    second_user_sign_up
+    expect(page).not_to have_content("Delete post")
+  end
+
+  scenario "Users can only delete a post they made themselves (from individual post page)" do
+    user_sign_up
+    create_post
+    log_out
+    second_user_sign_up
+    click_link "Hello, world!"
+    expect(page).not_to have_content("Delete post")
   end
 end
