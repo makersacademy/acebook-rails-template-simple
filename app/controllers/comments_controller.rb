90ac5e742b684d6261828716a4d42f2a04887997
Mayo Theodore <mayoelith@gmail.com>
2022-01-21 14:41:57 +0000

    comment saves to db without content

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,14 @@ class CommentsController < ApplicationController
   before_action :find_post
   
   def create
-    @post.comments.create(user_id: session[:current_user_id])
+    @post.comments.create(user_id: session[:current_user_id], content: "hello")
     redirect_to post_path(@post)
   end
 
   private
 
   def find_post
-    @post = Post.find(params[:post_id])
+    @post = Post.find_by(id: 1)
   end
-  
+
 end
