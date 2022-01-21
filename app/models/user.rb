44609d0fa9f1ffbcbc6d40d166b48836b00bc298
tc1316 <tituschang@hotmail.co.uk>
2022-01-21 11:17:17 +0000

    Added username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  validates :username, uniqueness: true
   has_one_attached :avatar
 end
