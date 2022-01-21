df1136f1e97cf67214ddf60ab23cdd6c1612d63b
zidehen <zoeidehen@gmail.com>
2022-01-21 11:05:38 +0000

    created web helpers for sign_up and create_post features

--- spec/features/user_can_delete_post_spec.rb
+++ spec/features/user_can_delete_post_spec.rb
@@ -3,27 +3,9 @@ require_relative '../webhelpers.rb'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can delete a post" do
-    # create_post
-    # Sign up ----------------------------
-    visit "/users"
-    click_link "Create new account"
-    fill_in "user[name]", with: "Zoe"
-    fill_in "user[email]", with: "zoe@gmail.com"
-    fill_in "user[password]", with: "123456"
-    fill_in "user[password_confirmation]", with: "123456"
-    click_button "Create User"
-
-    # Create post ------------------------
-    id = (User.find_by(email: "zoe@gmail.com")).id
-    visit "/posts"
-    click_link "New post"
-    fill_in "post[message]", with: "Hello, world!"
-    fill_in "post[user_id]", with: id
-    click_button "Submit"
-    visit '/posts'
-    expect(page).to have_link("Hello, world!")
-
-    # Delete post -------------------------
+    sign_up
+    create_post
+  
     visit "/posts"
     click_link "Delete post"
     expect(page).not_to have_content("Hello, world!")
--- spec/webhelpers.rb
+++ spec/webhelpers.rb
@@ -1,6 +1,20 @@
+def sign_up
+  visit "/users"
+  click_link "Create new account"
+  fill_in "user[name]", with: "Zoe"
+  fill_in "user[email]", with: "zoe@gmail.com"
+  fill_in "user[password]", with: "123456"
+  fill_in "user[password_confirmation]", with: "123456"
+  click_button "Create User" 
+end
+
 def create_post
+  id = (User.find_by(email: "zoe@gmail.com")).id
   visit "/posts"
   click_link "New post"
-  fill_in "Message", with: "Hello, world!"
+  fill_in "post[message]", with: "Hello, world!"
+  fill_in "post[user_id]", with: id
   click_button "Submit"
+  visit '/posts'
+  expect(page).to have_link("Hello, world!")
 end
