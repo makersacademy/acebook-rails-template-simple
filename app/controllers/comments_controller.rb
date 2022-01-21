db1b240ccdc02980c512acab575e5085509ee311
Sabrina <sabrinalord@msn.com>
2022-01-21 12:43:48 +0000

    fixed comments issue, comments can now be deleted and database updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,22 @@ class CommentsController < ApplicationController
     def new
       @comment = Comment.new
     end
+
     def create
-      p comment_params
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
         redirect_to posts_url
-      end
+    end
+
+      
+    def destroy
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+      redirect_to posts_url
+    end
     
-      private
+    private
         def comment_params
           params.require(:comment).permit(:content, :post_id, :user_id)
         end
