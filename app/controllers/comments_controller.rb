3a2f8574b246084530912fc888e420853acd72e4
Sabrina <sabrinalord@msn.com>
2022-01-23 15:50:21 +0000

    fixed security issue on post, user id now handled by controller. Not fixed yet for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
    
     def new
-      @comment = Comment.new
+      @comment = Comment.new()
     end
 
     def create
         @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(comment_params)
+         @comment = @post.comments.create(comment_params)
         redirect_to posts_url
     end
 
