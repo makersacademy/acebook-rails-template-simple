9589c78d1008037d8c6a87186c447c90df410eeb
Christopher <dagswebb@gmail.com>
2022-01-24 16:12:23 +0000

    Added Sign Out button

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -12,6 +12,12 @@ class HomepageController < ApplicationController
             redirect_to '/posts'
         end
     end
+
+    def destroy
+        session[:current_user_id] = nil
+            p session[:current_user_id]
+            redirect_to '/homepage'
+    end
     
     private
 
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   resources :photos
 
+  delete 'logout', to: 'homepage#destroy'
 end
 
 
