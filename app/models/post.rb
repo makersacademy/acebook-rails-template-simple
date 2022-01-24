99f4a202693c59c3d1ccbda71f74dbf2e0a2b383
Chris Clement <chrisclement@Chriss-Air.connect>
2022-01-24 17:14:50 +0000

    Post content changed minimum and max length. Text changed on the view for this too

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   # belongs_to :user
   has_one_attached :post_photo
-  validates :content, presence: true, length: { minimum: 10 }
+  validates :content, presence: true, length: { maximum: 150 }
   validates :users_id, presence: true
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
