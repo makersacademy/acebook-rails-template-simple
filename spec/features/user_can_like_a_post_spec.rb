8559db7aee0d864b3ca4abfba354b837e7942a8e
Chris Clement <chrisclement@Chriss-Air.connect>
2022-01-24 16:09:19 +0000

    Written feature test for liking, unliking and commenting on a post

--- spec/features/user_can_like_a_post_spec.rb
+++ spec/features/user_can_like_a_post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "Commenting on a post", type: :feature do
-  scenario "A user comments on a post and this is displayed in the comment section" do
+RSpec.feature "Liking a post", type: :feature do
+  scenario "A user can like a post. This adds to the number of Likes." do
     visit "/signup"
     fill_in "name", with: "Chris"
     fill_in "email", with: "chris@gmail.com"
@@ -16,9 +16,7 @@ RSpec.feature "Commenting on a post", type: :feature do
     click_link "Create new post"
     fill_in "post_content", with: "Hello, world!"
     click_button "Create Post"
-    click_link "Add a comment"
-    fill_in "content" , with: "Hello to you too"
-    click_button "Add comment"
-    expect(page).to have_content "Hello to you too"
+    click_button "Like"
+    expect(page).to have_content "1 Like"
   end
 end
