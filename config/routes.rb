e4968a633c3be94492db8187c852fdf6ca851b7f
kehinde alaka <kennyalaka93@gmail.com>
2022-01-25 14:16:56 +0000

    changed route of /users/index to /users. add this page to the navbar as list of users. KA/BW

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  match '/users',   to: 'users#index',   via: 'get'
   devise_for :users
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
