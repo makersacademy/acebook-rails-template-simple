8f31097f8ade9322b0f7715a51091bdbf6e3004c
zidehen <zoeidehen@gmail.com>
2022-01-25 12:36:13 +0000

    profile picture appears next to author of posts & comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     # @user.profile_picture ||= "/assets/images/default_profile_pic.jpg"
-    @user.profile_picture.attach(io: File.open("#{Rails.root}/app/assets/images/default_profile_pic.jpg"), filename: "default_profile_pic.jpg")
+    @user.profile_picture.attach(io: File.open("#{Rails.root}/app/assets/images/default_image.jpg"), filename: "default_image.jpg")
     if @user.save
       log_in @user
       redirect_to posts_path
