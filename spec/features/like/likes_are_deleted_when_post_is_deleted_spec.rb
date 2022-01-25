b7fcc7f17f3a0de5fe98b6d1c1df38d2a0a42653
Sarah <dousse.sarah@gmail.com>
2022-01-25 18:33:29 +0000

    fixing feature test likes_are_deleted_when_post_is_deleted

--- spec/features/like/likes_are_deleted_when_post_is_deleted_spec.rb
+++ spec/features/like/likes_are_deleted_when_post_is_deleted_spec.rb
@@ -16,14 +16,11 @@ RSpec.feature "Likes", type: :feature do
       visit("/") # log in page  => make a webhelpers method!!! or /login page?
       fill_in "session[email]", with: "fake@fake.com"
       fill_in "session[password]", with: "password123"
-      expect(page).to have_button "Log in"
-      click_button "Log in" # error is here ?!?! is this a button or a link ? can't log in
+      click_button "Log In" # error is here ?!?! is this a button or a link ? can't log in
 
       visit "/posts"
-      expect(page).to have_button "Log in"
-      expect(page).to have_content("Hello, world!")
       click_link "Hello, world!"
-      click_button "Delete"
+      click_link "Delete"
       expect(page).not_to have_content("Hello, world!")
       expect(page).not_to have_link "Like"
     end
