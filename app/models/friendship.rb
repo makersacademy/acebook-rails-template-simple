e958c7644789068f101cbedbc90e6a5ff7ab8183
zidehen <zoeidehen@gmail.com>
2022-01-26 17:08:49 +0000

    edited/tested the friendships controller

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,9 +3,11 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: "User"
 
   def self.create_reciprocal_for_ids(user_id, friend_id)
-    Friendship.create(user_id: user_id, friend_id: friend_id)
+    user_friendship = Friendship.create(user_id: user_id, friend_id: friend_id)
     
-    #[user_friendship]
+    friend_friendship = Friendship.create(user_id: friend_id, friend_id: user_id)
+    
+    [user_friendship, friend_friendship]
   end
-  
+
 end
