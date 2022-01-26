68231b37c9a28dcb59628497be78a8187e68d413
zidehen <zoeidehen@gmail.com>
2022-01-26 16:52:49 +0000

    change of plan- create_reciprocal_for_ids - like 4 easy steps

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,13 +2,17 @@ class FriendshipsController < ApplicationController
 
   def create
 
-    Friendship.create_reciprocal_for_ids(current_user.id, params[:friend_id])
+    
       # filter if that relationship already exists ? friendship btn gives params user_id == current_user and friend_id
         #if params.include?(friend_id) => true or false?
           # X Friendship.find_by(friend_id) => just first match
           # X Friendship.each do |f| f.user_id = 1 ? "lisa is my friend" : "no friend" end
-          # worked: user1.friendships => gives back all the friendships 
+          # worked: user1.friendships => gives back all the friendships  ONE ARRAY  user1.friendships.each do |f| p f.user.name end
           # current_user.friendships ?
-
+        friend = User.find(params[:friend_id])
+        test_arr = friend.friendships.map{ |f|
+          f.user.name == current_user.name    
+        }
+        if test_arr.include(true) ? p "you are already friends!" : Friendship.create_reciprocal_for_ids(current_user.id, params[:friend_id])
   end
 end
