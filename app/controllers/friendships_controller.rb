9b763c0fda564e2d2b4c145c06c46a40e36b8ae8
zidehen <zoeidehen@gmail.com>
2022-01-27 12:24:24 +0000

    changed default value in friendship table request column

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,25 +1,6 @@
-# class FriendshipsController < ApplicationController
-
-#   def create
-#       # filter if that relationship already exists ? friendship btn gives params user_id == current_user and friend_id
-#         #if params.include?(friend_id) => true or false?
-#           # X Friendship.find_by(friend_id) => just first match
-#           # X Friendship.each do |f| f.user_id = 1 ? "lisa is my friend" : "no friend" end
-#           # worked: user1.friendships => gives back all the friendships  ONE ARRAY  user1.friendships.each do |f| p f.user.name end
-#           # current_user.friendships ?
-#         friend = User.find(params[:friend_id])
-#         test_arr = friend.friendships.map{ |f|
-#           f.user.name == current_user.name    
-#         }
-#         if test_arr.include(true) ? p "you are already friends!" : Friendship.create_reciprocal_for_ids(current_user.id, params[:friend_id])
-#   end
-# end
-
 class FriendshipsController < ApplicationController
-    protect_from_forgery with: :exception
   def create
-      #is this bulk addition or individual addition?
-    #   if params.include?(:friend_id) #individual e.g. "Add friend" link
+      
         @new_friendships = Friendship.create_reciprocal_for_ids(current_user.id, params[:friend_id])
     #   end
       redirect_to posts_path
