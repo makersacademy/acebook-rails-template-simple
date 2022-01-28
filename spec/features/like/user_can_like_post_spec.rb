3f49930bea16db0486883b49512c031e68ced99f
EviePalaiochorinou <evangeliapal@gmail.com>
2022-01-28 12:09:28 +0000

    Fix all like feature tests

--- spec/features/like/user_can_like_post_spec.rb
+++ spec/features/like/user_can_like_post_spec.rb
@@ -1,11 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature "Likes", type: :feature do
+  describe "Like a specific post" do
+    scenario "Users can like his/her post from the news feed" do
+      user_sign_up
+      create_post
+      find(".like-button").click
+      expect(find(".likes")).to have_content "1 Like"
+    end
 
-  describe "Like a post from the news feed" do
+    scenario "Users can like a post from another user from the news feed" do
+      user_sign_up
+      create_post
+      log_out
+      second_user_sign_up
+      find(".like-button").click
+      expect(find(".likes")).to have_content "1 Like"
+    end
+  end
+
+  describe "Like a specific post" do
     scenario "Users can like his/her post" do
       user_sign_up
       create_post
+      click_link "Hello, world!"
       find(".like-button").click
       expect(find(".likes")).to have_content "1 Like"
     end
@@ -15,31 +33,9 @@ RSpec.feature "Likes", type: :feature do
       create_post
       log_out
       second_user_sign_up
-      find(".like-button").click #for like
-      expect(page).to have_button "Like"
+      click_link "Hello, world!"
+      find(".like-button").click
+      expect(find(".likes")).to have_content "1 Like"
     end
   end
-
-  describe "Like a specific post" do
-    scenario "Users can like his/her post" do
-      user_sign_up
-      create_post
-      click_link "Hello, world!"
-      click_button "Like"
-      click_button "Unlike"
-      expect(page).to have_button "Like"
-    end
-
-    scenario "Users can like a post from another user" do
-      user_sign_up
-      create_post
-      log_out
-      second_user_sign_up
-      click_link "Hello, world!"
-      click_button "Like"
-      click_button "Unlike"
-      expect(page).to have_button "Like"
-    end
-  end
-
 end
--- spec/features/like/user_can_unlike_post_spec.rb
+++ spec/features/like/user_can_unlike_post_spec.rb
@@ -1,41 +1,47 @@
 require 'rails_helper'
 
 RSpec.feature "Likes", type: :feature do
-  describe "Like a specific post" do
-    scenario "Users can unlike his/her post from the news feed" do
+
+  describe "Unlike a post from the news feed" do
+    scenario "Users can unlike his/her post" do
       user_sign_up
       create_post
-      click_button "Like"
-      expect(page).to have_button "Unlike"
+      find(".like-button").click #for like
+      find(".like-button").click #for unlike
+      expect(page).not_to have_content(".likes")
     end
 
-    scenario "Users can unlike a post from another user from the news feed" do
+    scenario "Users can unlike a post from another user" do
       user_sign_up
       create_post
       log_out
       second_user_sign_up
-      click_button "Like"
-      expect(page).to have_button "Unlike"
+      find(".like-button").click #for like
+      find(".like-button").click #for unlike
+      expect(page).not_to have_content(".likes")
     end
   end
 
-  describe "Like a specific post" do
-    scenario "Users can like his/her post" do
+  describe "Unlike a specific post" do
+    scenario "Users can unlike his/her post" do
       user_sign_up
       create_post
       click_link "Hello, world!"
-      click_button "Like"
-      expect(page).to have_button "Unlike"
+      find(".like-button").click #for like
+      find(".like-button").click #for unlike
+      expect(page).not_to have_content(".likes")
     end
 
-    scenario "Users can like a post from another user" do
+    scenario "Users can unlike a post from another user" do
       user_sign_up
       create_post
       log_out
       second_user_sign_up
       click_link "Hello, world!"
-      click_button "Like"
-      expect(page).to have_button "Unlike"
+      find(".like-button").click #for like
+      find(".like-button").click #for unlike
+      expect(page).not_to have_content(".likes")
     end
   end
+
 end
