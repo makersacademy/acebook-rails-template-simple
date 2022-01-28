340288a0c6127e10a91245d8a58650cbfe728d67
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-01-28 12:12:42 +0000

    button dynamically updated

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,7 @@
 class FriendshipsController < ApplicationController
   def create
+      @friend_id = params[:friend_id]
       @new_friendships = Friendship.create_reciprocal_for_ids(current_user.id, params[:friend_id])
-      redirect_to posts_path
+      do_not_run_js_in_test
   end
 end
