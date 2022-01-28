9823a8db26c0ea9529fffcdc979d5e6167c69e57
zidehen <zoeidehen@gmail.com>
2022-01-28 11:52:01 +0000

    passing test for can_add_friend feature test

--- spec/helpers/friendships_helper_spec.rb
+++ spec/helpers/friendships_helper_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the FriendshipsHelper. For example:
-#
-# describe FriendshipsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe FriendshipsHelper, type: :helper do
   it "should return true if two users are already friends" do
     user1 = User.create(name: "Zoe", email: "zoe@gmail.com", password: "123456", password_confirmation: "123456")
--- spec/webhelpers.rb
+++ spec/webhelpers.rb
@@ -1,6 +1,5 @@
 def sign_up
-  visit "/users"
-  click_link "Create new account"
+  visit("/users/new")
   fill_in "user[name]", with: "Zoe"
   fill_in "user[email]", with: "zoe@gmail.com"
   fill_in "user[password]", with: "123456"
