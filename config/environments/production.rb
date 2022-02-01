04f760781225f1a42000057a541c7da1af6ac1c1
Inimesh <ijmeshoulam@gmail.com>
2022-02-01 11:40:01 +0000

    Commenting out lines in config/environments/production.rb and config/storage.yml files to stop use of AWS storage in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Setting active storage service to amazon
-  config.active_storage.service = :amazon
+  # config.active_storage.service = :amazon
 end
